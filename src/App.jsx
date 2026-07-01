4df74e3c911bea6d08a3d474f1ea10d33454e487
abdulazizalmutlaaq <agflka123@gmail.com>
2026-07-01 12:05:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
+import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import AdminUsers from "./pages/AdminUsers";
 
@@ -24,7 +25,8 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={user ? <Navigate to="/" replace /> : <Login />} />
-      <Route path="/" element={<Gate><Dashboard /></Gate>} />
+      <Route path="/" element={<Gate><Projects /></Gate>} />
+      <Route path="/project/:projectId" element={<Gate><Dashboard /></Gate>} />
       <Route path="/users" element={<Gate adminOnly><AdminUsers /></Gate>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
